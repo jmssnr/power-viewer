57e48631492c113d80b1db5e014a834e636d2d42
jmssnr <maussner.johannes@gmail.com>
2024-06-21 17:21:26 +0200

    styles links and makes sidebar collapsible

--- src/app/(tools)/components/Sidebar/Sidebar.tsx
+++ src/app/(tools)/components/Sidebar/Sidebar.tsx
@@ -1,17 +1,82 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import classes from "./Sidebar.module.css";
+import { useState } from "react";
+import {
+  TransformIcon,
+  AngleIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from "@radix-ui/react-icons";
+
+const links = [
+  {
+    href: "/monitoring",
+    label: "Monitoring",
+    icon: <AngleIcon width={20} height={20} />,
+  },
+  {
+    href: "/analytics",
+    label: "Analytics",
+    icon: <TransformIcon width={20} height={20} />,
+  },
+];
 
 const Sidebar = () => {
+  const [open, setOpen] = useState(false);
+  const path = usePathname();
+
   return (
-    <aside className={classes.root}>
-      <ul>
-        <li>
-          <Link href="./monitoring">Monitoring</Link>
-        </li>
-        <li>
-          <Link href="./analytics">Analytics</Link>
-        </li>
-      </ul>
+    <aside className={open ? classes.root : classes.rootCollapsed}>
+      <div
+        style={{
+          display: "flex",
+          height: "100%",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          alignItems: "stretch",
+        }}
+      >
+        <div className={classes.list}>
+          {links.map((link, i) => (
+            <Link
+              key={i}
+              className={
+                path === link.href ? classes.linkHighlight : classes.link
+              }
+              href={link.href}
+            >
+              <span
+                style={{
+                  display: "flex",
+                  justifyContent: "space-evenly",
+                  alignItems: "center",
+                }}
+              >
+                {link.icon} {open && link.label}
+              </span>
+            </Link>
+          ))}
+        </div>
+        <button
+          style={{
+            width: 40,
+            height: 40,
+            alignSelf: "end",
+            display: "grid",
+            placeContent: "center",
+            color: "var(--accent-text)",
+            backgroundColor: "var(--accent-bg-active)",
+            border: "1px solid var(--accent-line)",
+            borderRadius: "5px",
+          }}
+          onClick={() => setOpen(!open)}
+        >
+          {open ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+        </button>
+      </div>
     </aside>
   );
 };
