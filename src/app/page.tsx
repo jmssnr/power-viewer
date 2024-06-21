84b375a5a53e1d6577e3162d387b05f66baf5955
jmssnr <maussner.johannes@gmail.com>
2024-06-21 11:53:40 +0200

    cleans up landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,14 @@
-"use client";
+import Link from "next/link";
 
-import { useGetPowerGeneration } from "@/hooks/useGetPowerGeneration";
-import CountrySelect from "./(tools)/components/CountrySelect";
-import PowerTypeSelect from "./(tools)/components/PowerTypeSelect";
-import DateToggleGroup from "./(tools)/components/DateToggleGroup/DateToggleGroup";
-import SimpleLineChart from "@/components/charts/cartesian/SimpleLineChart";
-import { useState } from "react";
-import PowerGenerationChart from "./(tools)/monitoring/components/PowerGenerationChart";
 export default function Home() {
-  const [powerType, setPowerType] = useState("");
-  const { data, isPending, isError } = useGetPowerGeneration();
-
-  console.log(powerType);
-  console.log(data);
-
-  return (
-    <main>
-      Landing Page
-      <div
-        style={{
-          padding: 50,
-          display: "flex",
-          flexDirection: "column",
-          gap: "10px",
-        }}
-      >
-        <DateToggleGroup />
-        <CountrySelect />
-        <PowerTypeSelect value={powerType} setValue={setPowerType} />
-        <PowerGenerationChart selection={powerType} />
-      </div>
-    </main>
-  );
+  <main>
+    <ul>
+      <li>
+        <Link href="./monitoring">Monitoring</Link>
+      </li>
+      <li>
+        <Link href="./analytics">Analytics</Link>
+      </li>
+    </ul>
+  </main>;
 }
