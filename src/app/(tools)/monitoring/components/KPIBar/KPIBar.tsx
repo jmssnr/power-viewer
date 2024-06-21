f80b021feaf3e6d63bba85f30bf1f532d8695c2b
jmssnr <maussner.johannes@gmail.com>
2024-06-21 16:02:06 +0200

    rounds numbers

--- src/app/(tools)/monitoring/components/KPIBar/KPIBar.tsx
+++ src/app/(tools)/monitoring/components/KPIBar/KPIBar.tsx
@@ -22,7 +22,7 @@ const valueHelper = (
     return "An error occured";
   }
 
-  return `${data[selection][key].toFixed(2)} MW`;
+  return `${data[selection][key].toFixed(0)} MW`;
 };
 
 const KPIBar = ({ selection }: KPIBarProps) => {
